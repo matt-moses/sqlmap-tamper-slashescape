084a7cb6ef0cd82fc0e3a9a58c4c82aed8b9aa40
Matt Moses <mmoses.web@gmail.com>
2019-04-10 00:06:37 -0500

    Update charescape.py

--- charescape.py
+++ charescape.py
@@ -3,6 +3,11 @@
 """
 Copyright (c) 2019 Matthew Moses
 See the file 'LICENSE' for copying permission
+
+[!] legal disclaimer:
+Developers assume no liability and are not responsible for any misuse or damage caused by this program.
+Use responsibly. Do good.
+
 """
 
 from lib.core.enums import PRIORITY
